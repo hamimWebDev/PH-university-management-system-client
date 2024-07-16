a3109c9c101c93138d75fc4ceaff1bd3ad4e7b29
hamimDev <mdhamimhowladerasif@gmail.com>
2024-07-15 20:37:26 -0600

    add Login, useNavigate & Sonner tost

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,16 +1,25 @@
-import { Layout } from "antd";
+import { Button, Layout } from "antd";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import { useAppDispatch } from "../../redux/features/hooks";
+import { logOut } from "../../redux/features/auth/authSlice";
 
 const { Header, Content } = Layout;
 
 const MainLayout = () => {
+  const disPatch = useAppDispatch();
+
+  const handleLogout = () => {
+    disPatch(logOut());
+  };
   return (
     <div>
       <Layout style={{ height: "100vh" }}>
         <Sidebar />
         <Layout>
-          <Header style={{ padding: 0 }} />
+          <Header>
+            <Button onClick={handleLogout}>Logout</Button>
+          </Header>
           <Content style={{ margin: "24px 16px 0" }}>
             <div
               style={{
